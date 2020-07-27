838ddc8d6e39416db696c1c49e085b06206b8724
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-27 16:36:40 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,7 +9,7 @@ variable "location" {
 
 variable "resource_group_name" {
   description = "The name of the resource group in which the resources are created"
-  default     = ""
+  default     = "myResourceGroup"
 }
 
 variable "application_port" {
