847758cef66afd85a397a986bf3a7fba596942e0
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-27 08:13:14 +0200

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,6 @@
+#File output.tf
+#https://docs.microsoft.com/en-us/azure/developer/terraform/create-vm-scaleset-network-disks-using-packer-hcl
+
+output "vmss_public_ip" {
+    value = azurerm_public_ip.vmss.fqdn
+}
