83071af49aff3c9463e958630fdee9704467e841
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-27 16:26:10 +0200

    Update vmss.tf

--- vmss.tf
+++ vmss.tf
@@ -132,12 +132,12 @@ resource "azurerm_virtual_machine_scale_set" "vmss" {
   }
 
   os_profile_linux_config {
-    disable_password_authentication = true
+    disable_password_authentication = false
 
-    ssh_keys {
-      path     = "/home/azureuser/.ssh/authorized_keys"
-      key_data = file("~/.ssh/id_rsa.pub")
-    }
+ #     ssh_keys {
+ #     path     = "/home/azureuser/.ssh/authorized_keys"
+ #     key_data = file("~/.ssh/id_rsa.pub")
+ #   }
   }
 
   network_profile {
